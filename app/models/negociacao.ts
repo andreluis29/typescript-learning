4262e72ec24f081f65baad90df2e76780756ec36
andreluis29 <andre.wenningkamp1@gmail.com>
2022-06-08 23:01:18 -0300

    Parte 2 concluída!

---
+++ app/models/negociacao.ts
@@ -0,0 +1,24 @@
+export class Negociacao {
+  private data;
+  private quantidade;
+  private valor;
+
+  constructor(data, quantidade, valor) {
+    this.data = data;
+    this.quantidade = quantidade;
+    this.valor = valor;
+  }
+  
+  getData() {
+    return this.data;
+  }
+
+  getQuantidade() {
+    return this.quantidade;
+  }
+
+  getValor() {
+    return this.valor;
+  }
+
+} 
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,26 +1,16 @@
 export class Negociacao {
-  #data;
-  #quantidade;
-  #valor;
-
-  constructor(data, quantidade, valor) {
-    this.#data = data;
-    this.#quantidade = quantidade;
-    this.#valor = valor;
-  }
-
-  get data() {
-    return this.#data;
-  }
-
-  get quantidade() {
-    return this.#quantidade;
-  }
-  
-  get valor() {
-    return this.#valor;
-  }
-  
-    
-  
+    constructor(data, quantidade, valor) {
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+    getData() {
+        return this.data;
+    }
+    getQuantidade() {
+        return this.quantidade;
+    }
+    getValor() {
+        return this.valor;
+    }
 }
